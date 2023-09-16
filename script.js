8eb0c33eee47a4c8f077b60890cd3ba73db6d42c
Mahitab24 <143906104+Mahitab24@users.noreply.github.com>
2023-09-16 07:03:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//document.onclick = function(){
+//alert("welcome to green");
+//}
+
+//function prepareEventHandlers() {
+	
+	//var myImage =  document.getElementsById("mainImage");
+    //myImage.onclick = function(){
+	//alert("explore more");
+//}
+	//const paras = document.querySelectorAll('P');
+	//paras.forEach(para=>{
+	//console.log(para);
+	//});
+   // console.log(paras[2]);
+
+const content = document.querySelector( '.content');
+console.log(content.innerHTML);
